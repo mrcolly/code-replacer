cb0d4db9560b264b75c44a12dbbb8067575ad7fc
alessio colombo <alessio.colombo@satispay.com>
2022-05-12 17:43:47 +0200

    added readme file

---
+++ readme.md
@@ -0,0 +1,37 @@
+# service generator
+
+this project is designed to replace every occurrences of words, based on a configuration, in:
++ directory names
++ file names
++ file content
+
+from an input directory or a git project
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Installation
+
+required:
++ [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+```sh
+npm i
+chmod 755 start.sh
+```
+
+## Usage
+
+add your configuration at `configuration.config.json`
+
++ to replace from a directory
+```
+./start.sh -i <directory path>
+```
+
++ to replace from a git project
+```
+./start.sh -g <git repo> -b <optional git branch>
+```
--- start.sh
+++ start.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 usage() {  
-    echo "Usage: $0 [-g <git repository to clone>] [-i <input folder>]"
-    echo "only one argument allowed" 1>&2
+    echo "Usage: $0 [-g <git repository to clone>] [-b <git branch name>] [-i <input folder>]"
+    echo "-g and -i are mutually exclusive" 1>&2
     exit 1
 }
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
-    usage;
+    usage
 fi
 
-while getopts ":g:i:" o; do
+while getopts ":g:b:i:" o; do
     case "${o}" in
         g)
             GIT=${OPTARG};;
+        b)
+            GIT_BRANCH=${OPTARG};;
         i)
             INPUT=${OPTARG}
             ;;
@@ -33,30 +35,35 @@ if [ ! -z ${GIT} ] && [ ! -z ${INPUT} ]; then
     usage
 fi
 
-GIT_INPUT=input;
-mkdir -p output;
+GIT_INPUT=input
+mkdir -p output
 
 function cleanup {
-  rm -rf ${GIT_INPUT};
+  rm -rf ${GIT_INPUT}
 }
 
-trap cleanup EXIT;
+trap cleanup EXIT
 
 if [ ! -z ${GIT} ]; then
     cd "$(dirname "$0")"
     rm -rf ${GIT_INPUT};
     mkdir -p ${GIT_INPUT};
     cd ${GIT_INPUT};
-    git clone ${GIT};
-    cd ..;
-    node index.js ${GIT_INPUT};
+    if [ ! -z ${GIT_BRANCH} ]; then
+        git clone ${GIT} -b ${GIT_BRANCH}
+    else
+        git clone ${GIT}
+    fi
+    
+    cd ..
+    node index.js ${GIT_INPUT}
 fi
 
 if [ ! -z ${INPUT} ]; then
-    if [ ! -d "${INPUT}" ]; then
-    echo "folder ${INPUT} does not exist!"
-    exit 1
+    if [ ! -d ${INPUT} ]; then
+        echo "folder ${INPUT} does not exist!"
+        exit 1
     fi
     cd "$(dirname "$0")"
-    node index.js ${INPUT};
+    node index.js ${INPUT}
 fi
